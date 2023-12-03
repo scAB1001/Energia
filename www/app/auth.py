98ef0d355f326fcb0a0f9f57b603457e180fef42
AB <bazigosa@gmail.com>
2023-12-03 16:03:12 +0000

    Commenting

--- www/app/auth.py
+++ www/app/auth.py
@@ -1,50 +1,36 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from flask_login import login_user, login_required, logout_user, current_user
-from app import db
-from .auth_service import authenticate_user, create_user, migrate_password
+from .auth_service import authenticate_and_login, register_and_login
 from .forms import LoginForm, RegistrationForm
-from sqlalchemy.exc import IntegrityError
 
 auth = Blueprint('auth', __name__)
-DANGER, SUCCESS = 'error', 'success'
+
+# Constants for messages and categories
+SUCCESS = 'success'
+ERROR = 'error'
+LOGOUT_SUCCESS = 'Signed out successfully!'
+INVALID_CREDENTIALS = 'Incorrect email or password, try again.'
+MAX_LOGIN_ATTEMPTS_REACHED = 'Maximum sign-in attempts reached.'
+
 MAX_LOGIN_ATTEMPTS = 3
-
-
-# Helper method to handle user login
-def handle_login(email, password):
-    user = authenticate_user(email, password)
-    if user:
-        migrate_password(user, password)  # Migrate password if needed
-        login_user(user, remember=True)
-        flash(f'Signed in successfully!', category=SUCCESS)
-        return True
-    else:
-        flash('Incorrect email or password, try again.', category=DANGER)
-        return False
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
-    # Check if max attempts reached and redirect to signup if so
     if session.get('login_attempts', 0) >= MAX_LOGIN_ATTEMPTS:
-        flash('Maximum sign-in attempts reached. ', category=DANGER)
+        flash(MAX_LOGIN_ATTEMPTS_REACHED, ERROR)
         return redirect(url_for('auth.signup'))
 
     if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-
-        if handle_login(email, password):
+        if authenticate_and_login(form.email.data, form.password.data):
             # Reset login attempts on successful login
             session['login_attempts'] = 0
             return redirect(url_for('views.home'))
         else:
-            # Increment the login attempt count
             session['login_attempts'] = session.get('login_attempts', 0) + 1
-            remaining_attempts = MAX_LOGIN_ATTEMPTS - session['login_attempts']
-            flash(f'You have {remaining_attempts+1} attempts remaining.', category=DANGER)
+            flash(INVALID_CREDENTIALS, ERROR)
 
     return render_template("/admin/login.html", form=form, user=current_user, title='Login')
 
@@ -53,58 +39,22 @@ def login():
 @login_required
 def logout():
     logout_user()
-    session['login_attempts'] = 0
-    flash(f'Signed out successfully!', category=SUCCESS)
-
+    session.pop('login_attempts', None)  # Clear login attempts
+    flash(LOGOUT_SUCCESS, SUCCESS)
     return redirect(url_for('views.home'))
-
-
-# Helper method to handle user registration
-def handle_registration(email, first_name, password1, password2):
-    if password1 != password2:
-        flash('Passwords do not match.', category=DANGER)
-        return False
-    elif len(password1) < 7:
-        flash('Password must be at least 7 characters.', category=DANGER)
-        return False
-    else:
-        try:
-            user = create_user(email, first_name, password1)
-        except IntegrityError:
-            db.session.rollback()
-            flash('An account with this email already exists.', category=DANGER)
-            return False
-        if user:
-            login_user(user, remember=True)
-            flash("Account created! We've signed you in.", category=SUCCESS)
-            return True
-        else:
-            flash('Unable to create an account at this time. Try again.', category=DANGER)
-            return False
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    #if not current_user.is_authenticated and session.get('login_attempts', 0) >= MAX_LOGIN_ATTEMPTS: 
-    #    flash(f'Please create a new account.', category=DANGER)
-
-    # If user is already logged in and wants to create another account,
-    #   they will be logged out of the initial upon creation
     if current_user.is_authenticated:
-        logout()
+        logout_user()  # Logout current user if logged in
 
     form = RegistrationForm()
     if form.validate_on_submit():
-        email = form.email.data
-        first_name = form.first_name.data
-        password1 = form.password.data
-        password2 = form.confirm_password.data
-
-        if handle_registration(email, first_name, password1, password2):
-            # Reset login attempts on successful sign-up and login
-            session['login_attempts'] = 0
+        if register_and_login(form.email.data, form.first_name.data, form.password.data, form.confirm_password.data):
+            # Reset login attempts on successful sign-up
+            session.pop('login_attempts', None)
             return redirect(url_for('views.home'))
-        else:
-            flash('Unable to create an account at this time. Try again.', category=DANGER)
+        # Error messages are handled within the register_and_login function
 
     return render_template("/admin/signup.html", form=form, user=current_user, title='Signup')
--- www/app/auth_service.py
+++ www/app/auth_service.py
@@ -1,18 +1,48 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
+from flask import flash
 from .models import User
 from . import db
+from sqlalchemy.exc import IntegrityError
 
-HASH_TYPE = 'pbkdf2:sha256'  # 'scrypt'
+# Constants for messages and categories
+SUCCESS = 'success'
+ERROR = 'error'
+PASSWORD_MISMATCH = 'Passwords do not match.'
+PASSWORD_LENGTH = 'Password must be at least 7 characters.'
+LOGIN_SUCCESS = 'Signed in successfully!'
+ACCOUNT_CREATED = "Account created! We've signed you in."
+ACCOUNT_EXISTS = 'An account with this email already exists.'
 
-def authenticate_user(email, password):
+HASH_TYPE = 'pbkdf2:sha256'
+
+
+def generate_hash(password):
+    return generate_password_hash(password, method=HASH_TYPE)
+
+
+def validate_password(password1, password2):
+    if password1 != password2:
+        flash(PASSWORD_MISMATCH, ERROR)
+        return False
+    if len(password1) < 7:
+        flash(PASSWORD_LENGTH)
+        return False
+    return True
+
+
+def authenticate_and_login(email, password):
     user = User.query.filter_by(email=email).first()
     if user and check_password_hash(user.password, password):
-        return user
-    return None
+        login_user(user, remember=True)
+        flash(LOGIN_SUCCESS, SUCCESS)
+        return True
+    else:
+        return False
 
 
 def create_user(email, first_name, password):
-    hashed_password = generate_password_hash(password, method=HASH_TYPE)
+    hashed_password = generate_password_hash(password)
     new_user = User(email=email, first_name=first_name,
                     password=hashed_password)
     db.session.add(new_user)
@@ -20,9 +50,15 @@ def create_user(email, first_name, password):
     return new_user
 
 
-def migrate_password(user, password, hash_type=HASH_TYPE):
-    if not user.password.startswith(HASH_TYPE) and check_password_hash(user.password, password):
-        user.password = generate_password_hash(password, method=hash_type)
-        db.session.commit()
-
-
+def register_and_login(email, first_name, password1, password2):
+    if not validate_password(password1, password2):
+        return False
+    try:
+        user = create_user(email, first_name, password1)
+        login_user(user, remember=True)
+        flash(ACCOUNT_CREATED, SUCCESS)
+        return True
+    except IntegrityError:
+        db.session.rollback()
+        flash(ACCOUNT_EXISTS, ERROR)
+        return False
